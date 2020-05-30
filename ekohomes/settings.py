b14564922c12301184b286577eb1addd8531d094
mofe64 <ogunbiyioladapo33@gmail.com>
2020-05-30 19:08:49 +0100

    frontend auth

--- ekohomes/settings.py
+++ ekohomes/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -34,6 +40,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
