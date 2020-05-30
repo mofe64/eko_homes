32a4e8fc22416025ac671f51b78aff91a62e5acc
mofe64 <ogunbiyioladapo33@gmail.com>
2020-05-30 13:45:10 +0100

    dynamic views for all pages

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -17,8 +18,49 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            if price == '250000000':
+                query_list = query_list.filter(price__gte=price)
+            else:
+                query_list = query_list.filter(price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': query_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
